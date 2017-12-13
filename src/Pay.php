d3af5322aaf518040f87ff3a7c9c59a5714db780
yansongda <me@yansongda.cn>
2017-12-14 00:20:18 +0800

    i

--- src/Pay.php
+++ src/Pay.php
@@ -4,5 +4,13 @@ namespace Yansongda\Pay;
 
 class Pay
 {
-    
+    public function make($method, $params)
+    {
+        
+    }
+
+    public static function __callStatic($method, $params)
+    {
+        return self::make($method, $params);
+    }
 }
