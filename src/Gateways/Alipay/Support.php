fbb1d9566cef4f28410ce9924620b3d6ebc083b8
yansongda <me@yansongda.cn>
2018-02-05 18:49:14 +0800

    fix #77

--- src/Gateways/Alipay/Support.php
+++ src/Gateways/Alipay/Support.php
@@ -75,7 +75,7 @@ class Support
         }
 
         throw new GatewayException(
-            'Get Alipay API Error:'.$result[$method]['msg'].' - '.$result[$method]['sub_code'],
+            'Get Alipay API Error:'.$result[$method]['msg'],
             $result[$method]['code'],
             $result
         );
