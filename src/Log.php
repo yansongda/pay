6e6ec24f32c6d5a3a93bfb2a3bcfa444494b5424
JimChen <18219111672@163.com>
2018-08-05 20:45:38 +0800

    perf: 添加日志工具类针对PHPStorm进行代码提示

--- src/Log.php
+++ src/Log.php
@@ -7,6 +7,18 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 
+/**
+ * @package Yansongda\Pay
+ * @method static void emergency($message, array $context = array())
+ * @method static void alert($message, array $context = array())
+ * @method static void critical($message, array $context = array())
+ * @method static void error($message, array $context = array())
+ * @method static void warning($message, array $context = array())
+ * @method static void notice($message, array $context = array())
+ * @method static void info($message, array $context = array())
+ * @method static void debug($message, array $context = array())
+ * @method static void log($message, array $context = array())
+ */
 class Log
 {
     /**
