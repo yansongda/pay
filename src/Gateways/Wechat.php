fe2708a127d71e0f543a2d8b5a624975344037ee
tanbo <721796631@qq.com>
2018-11-09 16:10:32 +0800

    移除非必要内容

--- src/Gateways/Wechat.php
+++ src/Gateways/Wechat.php
@@ -163,7 +163,7 @@ class Wechat implements GatewayApplicationInterface
      * @throws InvalidSignException
      * @throws \Yansongda\Pay\Exceptions\InvalidArgumentException
      *
-     * @return WeChatPayRefundCollection|WeChatOfficialAccountNoticeCollection
+     * @return Collection
      */
     public function verify($content = null, $refund = false): Collection
     {
