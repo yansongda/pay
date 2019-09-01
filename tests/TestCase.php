cc4f814b607a11be6b4e3d3cad15265000124724
yansongda <me@yansongda.cn>
2019-09-01 10:49:55 +0800

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,15 @@
 
 namespace Yansongda\Pay\Tests;
 
-use Mockery;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase
 {
     public function setUp()
     {
-        Mockery::globalHelpers();
     }
 
     public function tearDown()
     {
-        Mockery::close();
     }
 }
