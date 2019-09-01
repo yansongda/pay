d6997b347327f720eda7bc28d582cc03e7d626c0
yansongda <me@yansongda.cn>
2019-09-01 13:40:59 +0800

    tests

--- tests/PayTest.php
+++ tests/PayTest.php
@@ -4,6 +4,8 @@ namespace Yansongda\Pay\Tests;
 
 use Yansongda\Pay\Contracts\GatewayApplicationInterface;
 use Yansongda\Pay\Exceptions\InvalidGatewayException;
+use Yansongda\Pay\Gateways\Alipay;
+use Yansongda\Pay\Gateways\Wechat;
 use Yansongda\Pay\Pay;
 
 class PayTest extends TestCase
@@ -12,6 +14,7 @@ class PayTest extends TestCase
     {
         $alipay = Pay::alipay(['foo' => 'bar']);
 
+        $this->assertInstanceOf(Alipay::class, $alipay);
         $this->assertInstanceOf(GatewayApplicationInterface::class, $alipay);
     }
 
@@ -19,6 +22,7 @@ class PayTest extends TestCase
     {
         $wechat = Pay::wechat(['foo' => 'bar']);
 
+        $this->assertInstanceOf(Wechat::class, $wechat);
         $this->assertInstanceOf(GatewayApplicationInterface::class, $wechat);
     }
 
