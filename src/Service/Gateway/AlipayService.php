dac58e950604808c79a597a3d2a01e494f536fb6
yansongda <me@yansongda.cn>
2019-09-12 18:07:45 +0800

    update

--- src/Service/Gateway/AlipayService.php
+++ src/Service/Gateway/AlipayService.php
@@ -2,6 +2,21 @@
 
 namespace Yansongda\Pay\Service\Gateway;
 
-class AlipayService
+use Pimple\Container;
+use Yansongda\Pay\Contract\ServiceInterface;
+
+class AlipayService implements ServiceInterface
 {
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        // TODO: Implement register() method.
+    }
 }
