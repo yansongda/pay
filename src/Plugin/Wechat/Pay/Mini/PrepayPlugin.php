98d78e579a313330f8608ecb35bbeb073ccc01d9
yansongda <me@yansongda.cn>
2021-06-21 23:31:20 +0800

    update

--- src/Plugin/Wechat/Pay/Mini/PrepayPlugin.php
+++ src/Plugin/Wechat/Pay/Mini/PrepayPlugin.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Plugin\Wechat\Pay\Mini;
 
+use Yansongda\Supports\Config;
+
 class PrepayPlugin extends \Yansongda\Pay\Plugin\Wechat\Pay\Common\PrepayPlugin
 {
+    protected function getWechatId(Config $config): array
+    {
+        return [
+            'appid' => $config->get('mini_app_id', ''),
+            'mchid' => $config->get('mch_id', ''),
+        ];
+    }
 }
