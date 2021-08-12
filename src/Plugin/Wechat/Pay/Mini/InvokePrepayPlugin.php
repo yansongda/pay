2bb42b1c9524c151da2d3f6c2d57b37cf1b10b1f
Anthony <38497992+JieAnthony@users.noreply.github.com>
2021-08-12 10:43:25 +0800

    修复微信小程序支付的错误：支付验证签名失败
    重写 getAppid 使它正确的读取微信小程序的app id 而不是微信公众号的app id

--- src/Plugin/Wechat/Pay/Mini/InvokePrepayPlugin.php
+++ src/Plugin/Wechat/Pay/Mini/InvokePrepayPlugin.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Plugin\Wechat\Pay\Mini;
 
+use Yansongda\Pay\Rocket;
+
 class InvokePrepayPlugin extends \Yansongda\Pay\Plugin\Wechat\Pay\Common\InvokePrepayPlugin
 {
+    /**
+     * @param Rocket $rocket
+     * @return string
+     * @throws \Yansongda\Pay\Exception\ContainerDependencyException
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
+    protected function getAppid(Rocket $rocket): string
+    {
+        $config = get_wechat_config($rocket->getParams());
+
+        return $config->get('mini_app_id', '');
+    }
 }
