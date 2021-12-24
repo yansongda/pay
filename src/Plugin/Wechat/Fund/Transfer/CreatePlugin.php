ab4bda3d023e053518dcb0e0d45e0cc43c9b0208
yansongda <me@yansongda.cn>
2021-12-24 15:19:12 +0800

    feat: 收款用户姓名加密

--- src/Plugin/Wechat/Fund/Transfer/CreatePlugin.php
+++ src/Plugin/Wechat/Fund/Transfer/CreatePlugin.php
@@ -7,29 +7,39 @@ namespace Yansongda\Pay\Plugin\Wechat\Fund\Transfer;
 use Yansongda\Pay\Pay;
 use Yansongda\Pay\Plugin\Wechat\GeneralPlugin;
 use Yansongda\Pay\Rocket;
+use Yansongda\Supports\Config;
 
 class CreatePlugin extends GeneralPlugin
 {
     /**
      * @throws \Yansongda\Pay\Exception\ContainerDependencyException
      * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\InvalidConfigException
+     * @throws \Yansongda\Pay\Exception\InvalidParamsException
+     * @throws \Yansongda\Pay\Exception\InvalidResponseException
      * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
      */
     protected function doSomething(Rocket $rocket): void
     {
-        $config = get_wechat_config($rocket->getParams());
+        $params = $rocket->getParams();
+        $config = get_wechat_config($params);
 
-        $extra = [
-            'appid' => $config->get('mp_app_id'),
-        ];
-
-        if (Pay::MODE_SERVICE == $config->get('mode')) {
-            $extra = [
-                'sub_mchid' => $config->get('sub_mch_id', ''),
-            ];
+        if (empty($config->get('wechat_public_cert_path'))) {
+            reload_wechat_public_certs($params);
         }
 
-        $rocket->mergePayload($extra);
+        $certs = $config->get('wechat_public_cert_path');
+
+        if (empty($params['_serial_no'])) {
+            mt_srand();
+            $params['_serial_no'] = array_rand($certs);
+            $rocket->setParams($params);
+        }
+
+        $rocket->mergePayload($this->mergeAppId($config));
+        $rocket->mergePayload([
+            'transfer_detail_list' => $this->mergeEncryptUserName($params, $params['_serial_no'])
+        ]);
     }
 
     protected function getUri(Rocket $rocket): string
@@ -40,5 +50,41 @@ class CreatePlugin extends GeneralPlugin
     protected function getPartnerUri(Rocket $rocket): string
     {
         return 'v3/partner-transfer/batches';
+    }
+
+    protected function mergeAppId(Config $config): array
+    {
+        $appId = [
+            'appid' => $config->get('mp_app_id'),
+        ];
+
+        if (Pay::MODE_SERVICE == $config->get('mode')) {
+            $appId = [
+                'sub_mchid' => $config->get('sub_mch_id', ''),
+            ];
+        }
+
+        return $appId;
+    }
+
+    /**
+     * @throws \Yansongda\Pay\Exception\ContainerDependencyException
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\InvalidParamsException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
+    protected function mergeEncryptUserName(array $params, string $serialNo): array
+    {
+        $lists = $params['transfer_detail_list'] ?? [];
+
+        if (empty(reset($lists)['user_name'])) {
+            return $lists;
+        }
+
+        foreach ($lists as $key => $list) {
+            $lists[$key]['user_name'] = encrypt_wechat_contents($params, $list['user_name'], $serialNo);
+        }
+
+        return $lists;
     }
 }
