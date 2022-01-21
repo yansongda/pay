4388309db85f2b728483b1bbaf3fbbb35544f76a
yansongda <yansongda@users.noreply.github.com>
2022-01-21 12:21:06 +0800

    fix: 支持分账传递姓名 (#559)

--- src/Plugin/Wechat/Fund/Profitsharing/AddReceiverPlugin.php
+++ src/Plugin/Wechat/Fund/Profitsharing/AddReceiverPlugin.php
@@ -7,32 +7,71 @@ namespace Yansongda\Pay\Plugin\Wechat\Fund\Profitsharing;
 use Yansongda\Pay\Pay;
 use Yansongda\Pay\Plugin\Wechat\GeneralPlugin;
 use Yansongda\Pay\Rocket;
+use Yansongda\Pay\Traits\HasWechatEncryption;
+use Yansongda\Supports\Collection;
 
 class AddReceiverPlugin extends GeneralPlugin
 {
+    use HasWechatEncryption;
+
     /**
      * @throws \Yansongda\Pay\Exception\ContainerDependencyException
      * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\InvalidConfigException
+     * @throws \Yansongda\Pay\Exception\InvalidParamsException
+     * @throws \Yansongda\Pay\Exception\InvalidResponseException
      * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
      */
     protected function doSomething(Rocket $rocket): void
     {
+        $params = $rocket->getParams();
         $config = get_wechat_config($rocket->getParams());
+        $extra = $this->getWechatId($config, $rocket->getPayload());
 
-        $wechatId = [
-            'appid' => $config->get('mp_app_id'),
-        ];
+        if (!empty($params['receivers'][0]['name'] ?? '')) {
+            $params = $this->loadSerialNo($params);
 
-        if (Pay::MODE_SERVICE == $config->get('mode')) {
-            $wechatId['sub_mchid'] = $rocket->getPayload()
-                ->get('sub_mchid', $config->get('sub_mch_id', ''));
+            $rocket->setParams($params);
+
+            $extra['receivers'] = $this->getEncryptUserName($params);
         }
 
-        $rocket->mergePayload($wechatId);
+        $rocket->mergePayload($extra);
     }
 
     protected function getUri(Rocket $rocket): string
     {
         return 'v3/profitsharing/receivers/add';
+    }
+
+    protected function getWechatId(Collection $config, Collection $payload): array
+    {
+        $wechatId = [
+            'appid' => $config->get('mp_app_id'),
+        ];
+
+        if (Pay::MODE_SERVICE == $config->get('mode')) {
+            $wechatId['sub_mchid'] = $payload->get('sub_mchid', $config->get('sub_mch_id', ''));
+        }
+
+        return $wechatId;
+    }
+
+    /**
+     * @throws \Yansongda\Pay\Exception\ContainerDependencyException
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\InvalidParamsException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
+    protected function getEncryptUserName(array $params): array
+    {
+        $lists = $params['receivers'] ?? [];
+        $publicKey = $this->getPublicKey($params, $params['_serial_no'] ?? '');
+
+        foreach ($lists as $key => $list) {
+            $lists[$key]['name'] = encrypt_wechat_contents($list['name'], $publicKey);
+        }
+
+        return $lists;
     }
 }
