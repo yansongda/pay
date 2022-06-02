8c7913cc6a2a6eff452f7e061ee37a2231018eb5
yansongda <me@yansongda.cn>
2022-06-02 21:58:06 +0800

    feat: 微信服务商退款及查询退款支持自动 sub_mchid 参数

--- src/Plugin/Wechat/Pay/Common/FindRefundPlugin.php
+++ src/Plugin/Wechat/Pay/Common/FindRefundPlugin.php
@@ -25,6 +25,19 @@ class FindRefundPlugin extends GeneralPlugin
         return 'v3/refund/domestic/refunds/'.$payload->get('out_refund_no');
     }
 
+    /**
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
+    protected function getPartnerUri(Rocket $rocket): string
+    {
+        $params = $rocket->getParams();
+        $config = get_wechat_config($params);
+        $url = parent::getPartnerUri($rocket);
+
+        return $url.'?sub_mchid='.($params['sub_mchid'] ?? $config->get('sub_mch_id'));
+    }
+
     protected function getMethod(): string
     {
         return 'GET';
--- src/Plugin/Wechat/Pay/Common/RefundPlugin.php
+++ src/Plugin/Wechat/Pay/Common/RefundPlugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Plugin\Wechat\Pay\Common;
 
+use Yansongda\Pay\Pay;
 use Yansongda\Pay\Plugin\Wechat\GeneralPlugin;
 use Yansongda\Pay\Rocket;
 
@@ -14,7 +15,18 @@ class RefundPlugin extends GeneralPlugin
         return 'v3/refund/domestic/refunds';
     }
 
+    /**
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
     protected function doSomething(Rocket $rocket): void
     {
+        $config = get_wechat_config($rocket->getParams());
+
+        if (Pay::MODE_SERVICE == $config->get('mode')) {
+            $rocket->mergePayload([
+                'sub_mchid' => $rocket->getPayload()->get('sub_mchid', $config->get('sub_mch_id')),
+            ]);
+        }
     }
 }
