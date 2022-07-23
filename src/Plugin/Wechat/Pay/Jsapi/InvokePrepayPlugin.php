844d03dc1559785bd2d5ce49000bd455ee2f5a3e
小新 <1371606921@qq.com>
2022-07-23 15:21:01 +0800

    fix: 公众号服务商模式支付未返回正确的子商户公众号 appid

--- src/Plugin/Wechat/Pay/Jsapi/InvokePrepayPlugin.php
+++ src/Plugin/Wechat/Pay/Jsapi/InvokePrepayPlugin.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Plugin\Wechat\Pay\Jsapi;
 
+use Yansongda\Pay\Rocket;
+use Yansongda\Pay\Pay;
+
 class InvokePrepayPlugin extends \Yansongda\Pay\Plugin\Wechat\Pay\Common\InvokePrepayPlugin
 {
+    /**
+     * @throws \Yansongda\Pay\Exception\ContainerException
+     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
+     */
+    protected function getAppid(Rocket $rocket): string
+    {
+        $config = get_wechat_config($rocket->getParams());
+
+        if (Pay::MODE_SERVICE == $config->get('mode')) {
+            return $rocket->getPayload()->get('sub_appid', $config->get('sub_mp_app_id', ''));
+        }
+
+        return $config->get('mp_app_id', '');
+    }
 }
