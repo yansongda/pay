ae52a5e0d3b73a0160544696796ffc839fa9102a
yansongda <me@yansongda.cn>
2022-07-23 17:22:04 +0800

    update

--- src/Plugin/Wechat/Pay/Mini/InvokePrepayPlugin.php
+++ src/Plugin/Wechat/Pay/Mini/InvokePrepayPlugin.php
@@ -4,23 +4,10 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Plugin\Wechat\Pay\Mini;
 
-use Yansongda\Pay\Pay;
-use Yansongda\Pay\Rocket;
-
 class InvokePrepayPlugin extends \Yansongda\Pay\Plugin\Wechat\Pay\Common\InvokePrepayPlugin
 {
-    /**
-     * @throws \Yansongda\Pay\Exception\ContainerException
-     * @throws \Yansongda\Pay\Exception\ServiceNotFoundException
-     */
-    protected function getAppid(Rocket $rocket): string
+    protected function getConfigKey(): string
     {
-        $config = get_wechat_config($rocket->getParams());
-
-        if (Pay::MODE_SERVICE == $config->get('mode')) {
-            return $rocket->getPayload()->get('sub_appid', $config->get('mini_app_id', ''));
-        }
-
-        return $config->get('mini_app_id', '');
+        return 'mini_app_id';
     }
 }
