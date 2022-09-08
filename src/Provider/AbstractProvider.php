6f7e62bf3c833d296a88e9510b19143abad3a9fd
yansongda <me@yansongda.cn>
2022-09-08 18:57:28 +0800

    update

--- src/Provider/AbstractProvider.php
+++ src/Provider/AbstractProvider.php
@@ -20,9 +20,11 @@ use Yansongda\Pay\Exception\InvalidResponseException;
 use Yansongda\Pay\Logger;
 use Yansongda\Pay\Pay;
 use Yansongda\Pay\Rocket;
+
+use function Yansongda\Pay\should_do_http_request;
+
 use Yansongda\Supports\Collection;
 use Yansongda\Supports\Pipeline;
-use function Yansongda\Pay\should_do_http_request;
 
 abstract class AbstractProvider implements ProviderInterface
 {
