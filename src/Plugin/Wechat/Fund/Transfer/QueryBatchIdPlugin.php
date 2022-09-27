d4845a0b814c06381c44d03d0c919dbb844e2b3e
xiaohe4966 <496631085@qq.com>
2022-09-27 22:38:25 +0800

    修复微信批次单号查询批次单

--- src/Plugin/Wechat/Fund/Transfer/QueryBatchIdPlugin.php
+++ src/Plugin/Wechat/Fund/Transfer/QueryBatchIdPlugin.php
@@ -31,40 +31,15 @@ class QueryBatchIdPlugin extends GeneralPlugin
     {
         $payload = $rocket->getPayload();
 
-        if (is_null($payload->get('batch_id'))) {
+        if (is_null($payload->get('batch_id')) || is_null($payload->get('need_query_detail'))) {
             throw new InvalidParamsException(Exception::MISSING_NECESSARY_PARAMS);
         }
+        
+        $batchId = $payload->get('batch_id');
 
-        if ($payload->get('need_query_detail')=='true' || $payload->get('need_query_detail') == true) {
-            if (is_null($payload->get('detail_status'))) {
-                $payload->detail_status = 'ALL';
-            } else {
-                switch ($payload->get('detail_status')) {
-                    case 'ALL':
-                        //ALL:全部。需要同时查询转账成功和转账失败的明细单
-                        break;
-    
-                    case 'SUCCESS':
-                        //SUCCESS:转账成功。只查询转账成功的明细单
-                        break;
-    
-                    case 'FAIL':
-                        // FAIL:转账失败。只查询转账失败的明细单
-                        break;
-                    
-                    default:
-                            throw new InvalidParamsException(Exception::PARAMS_ERROR);
-                        break;
-                }
-            }
-        }else{
-            $payload->need_query_detail = 'false';
-        }
+        $payload->forget('batch_id');
 
-        $batch_id = $payload->get('batch_id');
-        unset($payload->batch_id);
-
-        return 'v3/transfer/batches/batch-id/'.$batch_id.
+        return 'v3/transfer/batches/batch-id/'.$batchId.
             '?'.$payload->query();
     }
 
