2b867169531ba6d11149b42b6250fad26c9e1082
xiaohe4966 <496631085@qq.com>
2022-09-28 11:24:46 +0800

    微信商家批次单号查询批次单时 query 参数不正确

--- src/Plugin/Wechat/Fund/Transfer/QueryOutBatchNoPlugin.php
+++ src/Plugin/Wechat/Fund/Transfer/QueryOutBatchNoPlugin.php
@@ -35,7 +35,11 @@ class QueryOutBatchNoPlugin extends GeneralPlugin
             throw new InvalidParamsException(Exception::MISSING_NECESSARY_PARAMS);
         }
 
-        return 'v3/transfer/batches/out-batch-no/'.$payload->get('out_batch_no').
+        $outBatchNo = $payload->get('out_batch_no');
+
+        $payload->forget('out_batch_no');
+
+        return 'v3/transfer/batches/out-batch-no/'.$outBatchNo.
             '?'.$payload->query();
     }
 
@@ -49,8 +53,12 @@ class QueryOutBatchNoPlugin extends GeneralPlugin
         if (is_null($payload->get('out_batch_no')) || is_null($payload->get('need_query_detail'))) {
             throw new InvalidParamsException(Exception::MISSING_NECESSARY_PARAMS);
         }
+        
+        $outBatchNo = $payload->get('out_batch_no');
 
-        return 'v3/partner-transfer/batches/out-batch-no/'.$payload->get('out_batch_no').
+        $payload->forget('out_batch_no');
+
+        return 'v3/partner-transfer/batches/out-batch-no/'.$outBatchNo.
             '?'.$payload->query();
     }
 }
