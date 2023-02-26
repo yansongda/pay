f949a74fa775f17efec22f2b3c1ce01cb7d8cff3
yansongda <me@yansongda.cn>
2023-02-26 19:51:37 +0800

    style: fix coding style

--- src/Plugin/Wechat/Risk/Complaints/CompleteComplaintPlugin.php
+++ src/Plugin/Wechat/Risk/Complaints/CompleteComplaintPlugin.php
@@ -7,9 +7,9 @@ namespace Yansongda\Pay\Plugin\Wechat\Risk\Complaints;
 use Yansongda\Pay\Exception\Exception;
 use Yansongda\Pay\Exception\InvalidParamsException;
 
-use Yansongda\Pay\Parser\OriginResponseParser;
 use function Yansongda\Pay\get_wechat_config;
 
+use Yansongda\Pay\Parser\OriginResponseParser;
 use Yansongda\Pay\Plugin\Wechat\GeneralPlugin;
 use Yansongda\Pay\Rocket;
 use Yansongda\Supports\Collection;
