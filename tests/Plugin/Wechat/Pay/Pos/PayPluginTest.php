4e4bbeeda7fdd87b9edd0812429874a0239a3c57
yansongda <me@yansongda.cn>
2023-03-19 21:28:18 +0800

    update

--- tests/Plugin/Wechat/Pay/Pos/PayPluginTest.php
+++ tests/Plugin/Wechat/Pay/Pos/PayPluginTest.php
@@ -33,8 +33,8 @@ class PayPluginTest extends \Yansongda\Pay\Tests\TestCase
 
         self::assertEquals(new Uri(Wechat::URL[Pay::MODE_NORMAL].'pay/micropay'), $radar->getUri());
         self::assertEquals('POST', $radar->getMethod());
-        self::assertEquals('1600314069', $payload->get('mchid'));
-        self::assertEquals('wx55955316af4ef13', $payload->get('app_id'));
+        self::assertEquals('1600314069', $payload->get('mch_id'));
+        self::assertEquals('wx55955316af4ef13', $payload->get('appid'));
         self::assertEquals('v2', $params['_version']);
         self::assertEquals([
             'Content-Type' => 'application/xml',
@@ -55,8 +55,8 @@ class PayPluginTest extends \Yansongda\Pay\Tests\TestCase
 
         self::assertEquals(new Uri(Wechat::URL[Pay::MODE_NORMAL].'pay/micropay'), $radar->getUri());
         self::assertEquals('POST', $radar->getMethod());
-        self::assertEquals('1600314069', $payload->get('mchid'));
-        self::assertEquals('yansongda', $payload->get('app_id'));
+        self::assertEquals('1600314069', $payload->get('mch_id'));
+        self::assertEquals('yansongda', $payload->get('appid'));
         self::assertEquals('v2', $params['_version']);
         self::assertEquals([
             'Content-Type' => 'application/xml',
--- tests/Plugin/Wechat/Shortcut/PosShortcutTest.php
+++ tests/Plugin/Wechat/Shortcut/PosShortcutTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Yansongda\Pay\Tests\Plugin\Wechat\Shortcut;
 
-use Yansongda\Pay\Exception\Exception;
-use Yansongda\Pay\Exception\InvalidParamsException;
 use Yansongda\Pay\Plugin\Wechat\Pay\Pos\PayPlugin;
 use Yansongda\Pay\Plugin\Wechat\Shortcut\PosShortcut;
 use Yansongda\Pay\Tests\TestCase;
@@ -26,14 +24,5 @@ class PosShortcutTest extends TestCase
         self::assertEquals([
             PayPlugin::class,
         ], $this->plugin->getPlugins([]));
-    }
-
-    public function testFoo()
-    {
-        self::expectException(InvalidParamsException::class);
-        self::expectExceptionCode(Exception::SHORTCUT_MULTI_TYPE_ERROR);
-        self::expectExceptionMessage('Query type [fooPlugins] not supported');
-
-        $this->plugin->getPlugins(['_type' => 'foo']);
     }
 }
