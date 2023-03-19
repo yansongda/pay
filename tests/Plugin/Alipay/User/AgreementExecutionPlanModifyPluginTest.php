24a6e0dc790fefac26593f3fd4e2d148f3220d3e
yansongda <me@yansongda.cn>
2023-03-19 19:20:08 +0800

    refactor: 重构 ArrayParser 类

--- tests/Plugin/Alipay/User/AgreementExecutionPlanModifyPluginTest.php
+++ tests/Plugin/Alipay/User/AgreementExecutionPlanModifyPluginTest.php
@@ -2,13 +2,14 @@
 
 namespace Yansongda\Pay\Tests\Plugin\Alipay\User;
 
+use Yansongda\Pay\Contract\ParserInterface;
 use Yansongda\Pay\Plugin\Alipay\User\AgreementExecutionPlanModifyPlugin;
 use Yansongda\Pay\Rocket;
 use Yansongda\Pay\Tests\TestCase;
 
 class AgreementExecutionPlanModifyPluginTest extends TestCase
 {
-    protected $plugin;
+    protected AgreementExecutionPlanModifyPlugin $plugin;
 
     protected function setUp(): void
     {
@@ -24,7 +25,7 @@ class AgreementExecutionPlanModifyPluginTest extends TestCase
 
         $result = $this->plugin->assembly($rocket, function ($rocket) { return $rocket; });
 
-        self::assertNull($result->getDirection());
+        self::assertEquals(ParserInterface::class, $result->getDirection());
         self::assertStringContainsString('alipay.user.agreement.executionplan.modify', $result->getPayload()->toJson());
     }
 }
--- tests/Plugin/Alipay/User/AgreementUnsignPluginTest.php
+++ tests/Plugin/Alipay/User/AgreementUnsignPluginTest.php
@@ -2,6 +2,7 @@
 
 namespace Yansongda\Pay\Tests\Plugin\Alipay\User;
 
+use Yansongda\Pay\Contract\ParserInterface;
 use Yansongda\Pay\Plugin\Alipay\User\AgreementUnsignPlugin;
 use Yansongda\Pay\Rocket;
 use Yansongda\Pay\Tests\TestCase;
@@ -24,7 +25,7 @@ class AgreementUnsignPluginTest extends TestCase
 
         $result = $this->plugin->assembly($rocket, function ($rocket) { return $rocket; });
 
-        self::assertNull($result->getDirection());
+        self::assertEquals(ParserInterface::class, $result->getDirection());
         self::assertStringContainsString('alipay.user.agreement.unsign', $result->getPayload()->toJson());
         self::assertStringContainsString('CYCLE_PAY_AUTH_P', $result->getPayload()->toJson());
     }
