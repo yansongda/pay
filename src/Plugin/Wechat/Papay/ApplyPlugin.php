7460666aa5b4f12dbf8728972655565db8afa077
李铭昕 <715557344@qq.com>
2023-04-09 14:31:42 +0800

    移除委托代扣的 Shortcut

--- src/Plugin/Wechat/Papay/ApplyPlugin.php
+++ src/Plugin/Wechat/Papay/ApplyPlugin.php
@@ -6,6 +6,8 @@ use Yansongda\Pay\Plugin\Wechat\GeneralV2Plugin;
 use Yansongda\Pay\Rocket;
 
 /**
+ * 申请代扣.
+ *
  * @see https://pay.weixin.qq.com/wiki/doc/api/wxpay_v2/papay/chapter3_8.shtml
  */
 class ApplyPlugin extends GeneralV2Plugin
--- src/Provider/Wechat.php
+++ src/Provider/Wechat.php
@@ -21,14 +21,12 @@ use Yansongda\Supports\Collection;
 use Yansongda\Supports\Str;
 
 /**
- * @method Collection app(array $order)        APP 支付
- * @method Collection mini(array $order)       小程序支付
- * @method Collection mp(array $order)         公众号支付
- * @method Collection scan(array $order)       扫码支付
- * @method Collection wap(array $order)        H5 支付
- * @method Collection transfer(array $order)   帐户转账
- * @method Collection papay(array $order)      支付时签约（委托代扣）
- * @method Collection papayApply(array $order) 申请代扣（委托代扣）
+ * @method Collection app(array $order)      APP 支付
+ * @method Collection mini(array $order)     小程序支付
+ * @method Collection mp(array $order)       公众号支付
+ * @method Collection scan(array $order)     扫码支付
+ * @method Collection wap(array $order)      H5 支付
+ * @method Collection transfer(array $order) 帐户转账
  */
 class Wechat extends AbstractProvider
 {
