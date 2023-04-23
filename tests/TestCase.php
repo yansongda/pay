c7383bb58abaf3007261e32e50eacb3aa2e03d8e
yansongda <me@yansongda.cn>
2023-04-23 20:34:15 +0800

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Yansongda\Pay\Tests;
 
-use DI\ContainerBuilder;
+use Hyperf\Pimple\ContainerFactory;
 use Hyperf\Utils\ApplicationContext;
 use Yansongda\Pay\Pay;
 
@@ -139,8 +139,8 @@ class TestCase extends \PHPUnit\Framework\TestCase
         ];
 
         // 未在 hyperf 框架内，所以 sdk 没有 container, 手动设置一个
-        if (class_exists(ApplicationContext::class) && class_exists(ContainerBuilder::class)) {
-            ApplicationContext::setContainer((new ContainerBuilder())->build());
+        if (class_exists(ApplicationContext::class) && class_exists(ContainerFactory::class)) {
+            ApplicationContext::setContainer((new ContainerFactory())());
         }
 
         Pay::config($config);
