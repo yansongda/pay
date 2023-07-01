759ddd8e17a7f443e7eb2f2bf433ea05fd41c025
yansongda <yansongda@users.noreply.github.com>
2023-07-01 23:31:22 +0800

    delete: 移除 `Yansongda\Pay\Direction\ArrayDirection` 类 (#819)

--- tests/Direction/CollectionDirectionTest.php
+++ tests/Direction/CollectionDirectionTest.php
@@ -3,9 +3,11 @@
 namespace Yansongda\Pay\Tests\Direction;
 
 use GuzzleHttp\Psr7\Response;
-use Yansongda\Pay\Packer\JsonPacker;
 use Yansongda\Pay\Direction\CollectionDirection;
-use Yansongda\Pay\Pay;
+use Yansongda\Pay\Exception\Exception;
+use Yansongda\Pay\Exception\InvalidResponseException;
+use Yansongda\Pay\Packer\JsonPacker;
+use Yansongda\Pay\Packer\QueryPacker;
 use Yansongda\Pay\Tests\TestCase;
 
 class CollectionDirectionTest extends TestCase
@@ -21,12 +23,59 @@ class CollectionDirectionTest extends TestCase
 
     public function testNormal()
     {
-        Pay::config();
-
         $response = new Response(200, [], '{"name": "yansongda"}');
 
         $result = $this->parser->parse(new JsonPacker(), $response);
 
         self::assertEquals(['name' => 'yansongda'], $result->all());
+    }
+
+    public function testResponseNull()
+    {
+        self::expectException(InvalidResponseException::class);
+        self::expectExceptionCode(Exception::RESPONSE_NONE);
+
+        $this->parser->parse(new JsonPacker(), null);
+    }
+
+    public function testWrongFormat()
+    {
+        self::expectException(InvalidResponseException::class);
+        self::expectExceptionCode(Exception::UNPACK_RESPONSE_ERROR);
+
+        $response = new Response(200, [], '{"name": "yansongda"}a');
+
+        $this->parser->parse(new JsonPacker(), $response);
+    }
+
+    public function testReadContents()
+    {
+        $response = new Response(200, [], '{"name": "yansongda"}');
+
+        $response->getBody()->read(2);
+
+        $result = $this->parser->parse(new JsonPacker(), $response);
+
+        self::assertEquals(['name' => 'yansongda'], $result->toArray());
+    }
+
+    public function testQueryBody()
+    {
+        $response = new Response(200, [], 'name=yansongda&age=29');
+
+        $result = $this->parser->parse(new QueryPacker(), $response);
+
+        self::assertEqualsCanonicalizing(['name' => 'yansongda', 'age' => '29'], $result->toArray());
+    }
+
+    public function testJsonWith()
+    {
+        $url = 'https://yansongda.cn?name=yansongda&age=29';
+
+        $response = new Response(200, [], json_encode(['h5_url' => $url]));
+
+        $result = $this->parser->parse(new JsonPacker(), $response);
+
+        self::assertEquals('https://yansongda.cn?name=yansongda&age=29', $result['h5_url']);
     }
 }
