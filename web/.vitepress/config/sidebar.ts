3794d6ec7a9aaba8cb1557f96bd625f2b4021a6a
yansongda <me@yansongda.cn>
2023-09-10 12:47:46 +0800

    fix(docs): fix docs sidebar

--- web/.vitepress/config/sidebar.ts
+++ web/.vitepress/config/sidebar.ts
@@ -1,13 +1,10 @@
-import versions from '../sidebar/versions'
+// @ts-nocheck
 import { DefaultTheme } from 'vitepress'
 
-let sidebars: DefaultTheme.Sidebar = {}
-
-versions.forEach(async (version) => {
-  const path = `/docs/${version}/`
-
-  // @ts-ignore
-  sidebars[path] = (await import('../sidebar/v3')).default as DefaultTheme.Sidebar
-});
+let sidebars: DefaultTheme.Sidebar = {
+  '/docs/v1': (await import('../sidebar/v1')).default as DefaultTheme.Sidebar,
+  '/docs/v2': (await import('../sidebar/v2')).default as DefaultTheme.Sidebar,
+  '/docs/v3': (await import('../sidebar/v3')).default as DefaultTheme.Sidebar,
+}
 
 export default sidebars
