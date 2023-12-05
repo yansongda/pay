66c7c21e5833158c68a6705bd9084dfe67945a30
yansongda <yansongda@users.noreply.github.com>
2023-12-05 10:23:59 +0800

    docs: update (#877)

--- web/.vitepress/theme/components/Docs/AsideOutlineAfter.vue
+++ web/.vitepress/theme/components/Docs/AsideOutlineAfter.vue
@@ -5,7 +5,13 @@
     link: string
   }
 
-  const companies: Company[] = []
+  const companies: Company[] = [
+    {
+      name: 'df81.com',
+      link: 'https://df81.com/',
+      image: '/images/companies/df81.png'
+    },
+  ]
 </script>
 
 <template>
