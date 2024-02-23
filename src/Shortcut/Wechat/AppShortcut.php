e428befafba325461621170c4343871190f07e94
yansongda <me@yansongda.cn>
2024-02-23 12:02:48 +0800

    fix: 修复微信 App 支付参数异常问题

--- src/Shortcut/Wechat/AppShortcut.php
+++ src/Shortcut/Wechat/AppShortcut.php
@@ -7,12 +7,12 @@ namespace Yansongda\Pay\Shortcut\Wechat;
 use Yansongda\Artful\Contract\ShortcutInterface;
 use Yansongda\Artful\Plugin\AddPayloadBodyPlugin;
 use Yansongda\Artful\Plugin\ParserPlugin;
-use Yansongda\Pay\Plugin\Alipay\V2\Pay\App\PayPlugin;
 use Yansongda\Pay\Plugin\Wechat\AddRadarPlugin;
 use Yansongda\Pay\Plugin\Wechat\ResponsePlugin;
 use Yansongda\Pay\Plugin\Wechat\StartPlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\AddPayloadSignaturePlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\Pay\App\InvokePlugin;
+use Yansongda\Pay\Plugin\Wechat\V3\Pay\App\PayPlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\VerifySignaturePlugin;
 
 class AppShortcut implements ShortcutInterface
--- tests/Shortcut/Wechat/AppShortcutTest.php
+++ tests/Shortcut/Wechat/AppShortcutTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Yansongda\Pay\Tests\Shortcut\Wechat;
 
 use Yansongda\Artful\Plugin\AddPayloadBodyPlugin;
-use Yansongda\Pay\Plugin\Alipay\V2\Pay\App\PayPlugin;
 use Yansongda\Artful\Plugin\ParserPlugin;
 use Yansongda\Pay\Plugin\Wechat\AddRadarPlugin;
 use Yansongda\Pay\Plugin\Wechat\ResponsePlugin;
 use Yansongda\Pay\Plugin\Wechat\StartPlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\AddPayloadSignaturePlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\Pay\App\InvokePlugin;
+use Yansongda\Pay\Plugin\Wechat\V3\Pay\App\PayPlugin;
 use Yansongda\Pay\Plugin\Wechat\V3\VerifySignaturePlugin;
 use Yansongda\Pay\Shortcut\Wechat\AppShortcut;
 use Yansongda\Pay\Tests\TestCase;
