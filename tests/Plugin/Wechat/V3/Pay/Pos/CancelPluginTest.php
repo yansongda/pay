a299083321d1ad7488cd2e617709b7776dd82ac6
yansongda <me@yansongda.cn>
2024-04-27 22:01:17 +0800

    update

--- tests/Plugin/Wechat/V3/Pay/Pos/CancelPluginTest.php
+++ tests/Plugin/Wechat/V3/Pay/Pos/CancelPluginTest.php
@@ -2,6 +2,8 @@
 
 namespace Yansongda\Pay\Tests\Plugin\Wechat\V3\Pay\Pos;
 
+use Yansongda\Artful\Exception\InvalidParamsException;
+use Yansongda\Pay\Exception\Exception;
 use Yansongda\Pay\Plugin\Wechat\V3\Pay\Pos\CancelPlugin;
 use Yansongda\Artful\Rocket;
 use Yansongda\Pay\Tests\TestCase;
@@ -35,5 +37,19 @@ class CancelPluginTest extends TestCase
             'appid' => 'wx55955316af4ef13',
             'mchid' => '1600314069'
         ], $payload->all());
+    }
+
+    public function testMissingOut()
+    {
+        $rocket = new Rocket();
+        $rocket->setPayload(new Collection([
+            'aaa' => 'aaa'
+        ]));
+
+        self::expectException(InvalidParamsException::class);
+        self::expectExceptionCode(Exception::PARAMS_NECESSARY_PARAMS_MISSING);
+        self::expectExceptionMessage('参数异常: 付款码支付撤销订单，参数缺少 `out_trade_no`');
+
+        $this->plugin->assembly($rocket, function ($rocket) { return $rocket; });
     }
 }
