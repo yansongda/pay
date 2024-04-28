f453c2b7edbba4cd370e318c344223c1ad74b2f3
ohh <mrmercury@foxmail.com>
2024-04-28 11:40:22 +0800

    V2版本 微信发放普通红包

---
+++ src/Shortcut/Wechat/RedpackShortcut.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yansongda\Pay\Shortcut\Wechat;
+
+use Yansongda\Artful\Contract\ShortcutInterface;
+use Yansongda\Artful\Plugin\AddPayloadBodyPlugin;
+use Yansongda\Artful\Plugin\ParserPlugin;
+use Yansongda\Pay\Plugin\Wechat\AddRadarPlugin;
+use Yansongda\Pay\Plugin\Wechat\ResponsePlugin;
+use Yansongda\Pay\Plugin\Wechat\StartPlugin;
+use Yansongda\Pay\Plugin\Wechat\V2\AddPayloadSignaturePlugin;
+use Yansongda\Pay\Plugin\Wechat\V2\Pay\Redpack\SendPlugin;
+use Yansongda\Pay\Plugin\Wechat\V2\VerifySignaturePlugin;
+
+class RedpackShortcut implements ShortcutInterface
+{
+    public function getPlugins(array $params): array
+    {
+        return [
+            StartPlugin::class,
+            SendPlugin::class,
+            AddPayloadSignaturePlugin::class,
+            AddPayloadBodyPlugin::class,
+            AddRadarPlugin::class,
+            VerifySignaturePlugin::class,
+            ResponsePlugin::class,
+            ParserPlugin::class,
+        ];
+    }
+}
