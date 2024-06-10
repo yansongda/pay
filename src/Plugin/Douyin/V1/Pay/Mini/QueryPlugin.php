9d168bc426380625082b62809d1343c40becac2e
yansongda <me@yansongda.cn>
2024-06-10 23:01:17 +0800

    update

---
+++ src/Plugin/Douyin/V1/Pay/Mini/QueryPlugin.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yansongda\Pay\Plugin\Douyin\V1\Pay\Mini;
+
+use Closure;
+use Yansongda\Artful\Contract\PluginInterface;
+use Yansongda\Artful\Exception\ContainerException;
+use Yansongda\Artful\Exception\InvalidParamsException;
+use Yansongda\Artful\Exception\ServiceNotFoundException;
+use Yansongda\Artful\Logger;
+use Yansongda\Artful\Rocket;
+use Yansongda\Pay\Exception\Exception;
+use Yansongda\Pay\Pay;
+use Yansongda\Supports\Collection;
+
+use function Yansongda\Pay\get_provider_config;
+
+/**
+ * @see https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/pay-list/query
+ */
+class QueryPlugin implements PluginInterface
+{
+    /**
+     * @throws ContainerException
+     * @throws InvalidParamsException
+     * @throws ServiceNotFoundException
+     */
+    public function assembly(Rocket $rocket, Closure $next): Rocket
+    {
+        Logger::debug('[Douyin][V1][Pay][Mini][QueryPlugin] 插件开始装载', ['rocket' => $rocket]);
+
+        $payload = $rocket->getPayload();
+        $params = $rocket->getParams();
+        $config = get_provider_config('douyin', $params);
+
+        if (is_null($payload)) {
+            throw new InvalidParamsException(Exception::PARAMS_NECESSARY_PARAMS_MISSING, '参数异常: 抖音小程序查询订单，参数为空');
+        }
+
+        if (Pay::MODE_SERVICE === ($config['mode'] ?? Pay::MODE_NORMAL)) {
+            $data = $this->service($payload, $config);
+        }
+
+        $rocket->mergePayload(array_merge(
+            [
+                '_method' => 'POST',
+                '_url' => 'api/apps/ecpay/v1/query_order',
+                'app_id' => $config['mini_app_id'] ?? '',
+            ],
+            $data ?? [],
+        ));
+
+        Logger::info('[Douyin][V1][Pay][Mini][QueryPlugin] 插件装载完毕', ['rocket' => $rocket]);
+
+        return $next($rocket);
+    }
+
+    protected function service(Collection $payload, array $config): array
+    {
+        return [
+            'thirdparty_id' => $payload->get('thirdparty_id', $config['thirdparty_id'] ?? ''),
+        ];
+    }
+}
---
+++ src/Shortcut/Douyin/QueryShortcut.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yansongda\Pay\Shortcut\Douyin;
+
+use Yansongda\Artful\Contract\ShortcutInterface;
+use Yansongda\Artful\Exception\InvalidParamsException;
+use Yansongda\Artful\Plugin\AddPayloadBodyPlugin;
+use Yansongda\Artful\Plugin\ParserPlugin;
+use Yansongda\Artful\Plugin\StartPlugin;
+use Yansongda\Pay\Exception\Exception;
+use Yansongda\Pay\Plugin\Douyin\V1\Pay\AddPayloadSignaturePlugin;
+use Yansongda\Pay\Plugin\Douyin\V1\Pay\AddRadarPlugin;
+use Yansongda\Pay\Plugin\Douyin\V1\Pay\Mini\QueryPlugin as MiniQueryPlugin;
+use Yansongda\Pay\Plugin\Douyin\V1\Pay\ResponsePlugin;
+use Yansongda\Supports\Str;
+
+class QueryShortcut implements ShortcutInterface
+{
+    /**
+     * @throws InvalidParamsException
+     */
+    public function getPlugins(array $params): array
+    {
+        $action = Str::camel($params['_action'] ?? 'default').'Plugins';
+
+        if (method_exists($this, $action)) {
+            return $this->{$action}();
+        }
+
+        throw new InvalidParamsException(Exception::PARAMS_SHORTCUT_ACTION_INVALID, "Query action [{$action}] not supported");
+    }
+
+    protected function defaultPlugins(): array
+    {
+        return $this->miniPlugins();
+    }
+
+    protected function miniPlugins(): array
+    {
+        return [
+            StartPlugin::class,
+            MiniQueryPlugin::class,
+            AddPayloadSignaturePlugin::class,
+            AddPayloadBodyPlugin::class,
+            AddRadarPlugin::class,
+            ResponsePlugin::class,
+            ParserPlugin::class,
+        ];
+    }
+}
